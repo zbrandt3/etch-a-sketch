e22b8d9ff44148d3f02e15baf4001410ae9fc150
Zane Brandt <zbrandt3@gmail.com>
2024-12-13 14:59:41 -0600

    refactor events and change grid style

--- script.js
+++ script.js
@@ -3,21 +3,34 @@ const clearBtn = document.querySelector("#clear-btn");
 const gridInput = document.querySelector("#grid-input");
 const submitBtn = document.querySelector("#submit-btn");
 const gridContainer = document.querySelector(".grid");
+const gridInitVal = 8;
+const gridwidth = gridContainer.clientHeight;
 
+gridInput.value = gridInitVal;
 addDrawEvent();
+drawGrid()
+submitBtn.addEventListener("click", drawGrid);
+clearBtn.addEventListener("click", clear);
 
-submitBtn.addEventListener("click", () => {
+function addDrawEvent() {
+    Array.from(divs).forEach((div) => {
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "blue";
+        })
+    })
+}
+
+function drawGrid() {
     gridSize = gridInput.value;
+    gridInput.value = "";
     
     if(gridSize > 20) {
         alert("Too beeg");
-        gridInput.value = "";
         return;
     }
     
     if(gridSize < 2) {
         alert("Too smol");
-        gridInput.value = "";
         return;
     }
 
@@ -28,52 +41,25 @@ submitBtn.addEventListener("click", () => {
     for(var i = 0; i < gridSize; i++) {
         var newRow = document.createElement("div");
         newRow.classList.add("row");
+        newRow.style.height = gridwidth/gridSize;
+        newRow.style.width = gridwidth;
         gridContainer.appendChild(newRow);
+        console.log(newRow.style.height)
         
 
         for(var j = 0; j < gridSize; j++) {
             var newCol = document.createElement("div")
             newCol.classList.add("column");
+            newCol.style.height = gridwidth/gridSize;
+            newCol.style.width = gridwidth/gridSize;
             newRow.appendChild(newCol)
         }
     }
 
     divs = document.querySelectorAll(".column");
     addDrawEvent();
-
-})
-
-clearBtn.addEventListener("click", () => {
-    Array.from(divs).forEach((div) => {
-        div.style.backgroundColor = "white";
-    })
-});
-
-
-
-function addDrawEvent() {
-    Array.from(divs).forEach((div) => {
-        div.addEventListener("mouseover", () => {
-            div.style.backgroundColor = "blue";
-        })
-    })
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//TODO: Figure out how to incorporate functions within eventlisteners
 function etch(element) {
     element.style.backgroundColor = "blue";
 }
